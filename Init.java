85357453313985bf683a716ff643d21361fac236
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-07-04 19:25:53 -0400

    logic

--- Init.java
+++ Init.java
@@ -2,13 +2,24 @@
 public class Init {
     public static void main(String[] args) {
 
-        int dato1, dato2, dato3, grupal;
+        int dato1 = 20;
+        int dato2 = 35;
+        int aux1;
 
-        dato1 = 5;
-        dato2 = 15;
-        dato3 = 25;
+        System.out.println("Antes del intercambio");
+        System.out.println("El valor de dato1 es: " + dato1);
+        System.out.println("El valor de dato2 es: " + dato2);
 
-        grupal = dato1 + dato2 - dato3;
-        System.out.println("El resultado de los tres datos es: " + grupal);
+        //0    //20
+        aux1 = dato1;
+        //20   //35
+        dato1 = dato2;
+        //35   //20
+        dato2 = aux1;
+
+        System.out.println("Despues del intercambio");
+        System.out.println("El valor de dato1 es: " + dato1);
+        System.out.println("El valor de dato2 es: " + dato2);
     }
+
 }
