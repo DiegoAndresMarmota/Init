467b3e4f86644329ccc53c95103fcf18b2c39b8f
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-07-04 19:10:11 -0400

    first commit

---
+++ Init.java
@@ -0,0 +1,14 @@
+
+public class Init {
+    public static void main(String[] args) {
+
+        int dato1, dato2, dato3, grupal;
+
+        dato1 = 5;
+        dato2 = 15;
+        dato3 = 25;
+
+        grupal = dato1 + dato2 - dato3;
+        System.out.println("El resultado de los tres datos es: " + grupal);
+    }
+}
